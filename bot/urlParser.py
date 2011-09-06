9f7afcbb627aa8c7d2d21fcbf1c1e952a9a8ecc3
Henry Barnett <PintSizedCat@gmail.com>
2011-09-06 17:48:23 +0100

    Changed it so that nick is the actual nick rather than Nick Ident and Host

--- bot/urlParser.py
+++ bot/urlParser.py
@@ -33,7 +33,11 @@ def parse(nick, channel, message):
         elif isinstance(val, Url):
             # Set up the values that we don't pass to the process method
             val.msg = message
-            val.nick = nick
+            idx = nick.find("!")
+            if idx == -1:
+                val.nick = nick
+            else
+                val.nick = nick[:idx]
             val.channel = channel
             # Add to the database session
             print "msg:%s\nurl:%s\nnick:%s\nchannel:%s\ntitle:%s\npgType:%s\nlType:%s\nfType:%s\n" % (val.msg, val.url, val.nick, val.channel, val.title, val.page_type, val.link_type, val.file_type)
