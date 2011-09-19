4ec354eeaf1885608f44f395e8e863004e911688
Anthony Plunkett <anthony@thefort.org>
2011-09-19 15:12:16 -0400

    Url has a conversation property, showing -2 +2 messages around link.

--- model.py
+++ model.py
@@ -126,13 +126,21 @@ class Url(Base):
     img_thumb  = Column(Text)
     message_id = Column(Integer, ForeignKey(Message.id))
     date_created = Column(DateTime, default=datetime.now())
-    
+
     def __init__(self, url):
         self.url = url
         self.date_created = datetime.now()
         
     def __repr__(self):
         return "<URL: %s>" % (self.url)    
+
+    @property
+    def conversation(self):
+        m = self.message_id
+        nc = self.message.network_channel
+        before = Message.query.filter_by(network_channel = nc).filter(Message.id < self.message_id).order_by(Message.id.desc()).limit(2)
+        after = Message.query.filter_by(network_channel = nc).filter(Message.id > self.message_id).order_by(Message.id.asc()).limit(2)
+        return list(before) + [self.message] + list(after)
 
 class Admin(Base, UserMixin):
     __tablename__ = 'admin'
