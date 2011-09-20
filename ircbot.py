7a232efd26b48738363d7b9fb4a9690bee8582c6
Henry Barnett <PintSizedCat@gmail.com>
2011-09-21 00:26:24 +0100

    Added ability to connect to more than one server.

--- ircbot.py
+++ ircbot.py
@@ -3,8 +3,8 @@ from bot.ScudBot import ScudBotFactory
 from model import Bot
 
 if __name__ == "__main__":
-    bot = Bot.query.first()
-    print "Connecting to %s:%s" % (bot.network.server, bot.network.port)
-    botObj = ScudBotFactory(bot)
-    reactor.connectTCP(bot.network.server, bot.network.port,botObj)
+    for bot in Bot.query.all():
+        print "Connecting to %s:%s" % (bot.network.server, bot.network.port)
+        botObj = ScudBotFactory(bot)
+        reactor.connectTCP(bot.network.server, bot.network.port,botObj)
     reactor.run()
