a4791060a85fc6662c75f56b77fef95380710f43
Anthony Plunkett <anthony@thefort.org>
2011-09-30 18:21:33 -0400

    Url object now checks for previous mentions.

--- model.py
+++ model.py
@@ -112,7 +112,22 @@ class Message(Base):
         
     def __repr__(self):
         return "%s" % (self.message)
-    
+
+def check_existing(context):
+    ''' Checks database to see if url link has been mentioned before.
+
+        When a Url object is created, this function checks the existing
+        database to see if the link has been mentioned before, if it
+        has, it returns the 'original' Url id, which is stored against
+        the new Url.
+        
+    '''
+
+    item = Url.query.filter_by(url=context.current_parameters['url']).filter(Url.original_id == None).first()
+    if item:
+        return item.id
+    return None
+
 class Url(Base):
     ''' Logs Links '''
     __tablename__ = 'url'
@@ -126,13 +141,14 @@ class Url(Base):
     img_thumb  = Column(Text)
     message_id = Column(Integer, ForeignKey(Message.id))
     date_created = Column(DateTime, default=datetime.now())
+    original_id = Column(Integer, ForeignKey("url.id"), default=check_existing)
 
     def __init__(self, url):
         self.url = url
         self.date_created = datetime.now()
-        
+
     def __repr__(self):
-        return "<URL: %s>" % (self.url)    
+        return "<URL: %s>" % (self.url)
 
     @property
     def conversation(self):
