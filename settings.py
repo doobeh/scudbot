7c791645908c5fce6de3543c4254b1ce1387735f
Anthony Plunkett <anthony@thefort.org>
2013-03-23 23:23:24 -0400

    Changed to read config from settings.py and PEP8 formatting fixes

--- settings.py
+++ settings.py
@@ -1,7 +1,15 @@
-# Filesystem Related
+import os
 
-IMAGE_DIR = '/home/scud/scudbot/static/img/'
-THUMB_DIR = '/home/scud/scudbot/static/img/thumb/'
+# Helper to keep paths relative.
+_basedir = os.path.abspath(os.path.dirname(__name__))
+
+# Flask General Settings
+SECRET_KEY = 'SOMETHING_UNGUESSABLE'
+DEBUG = True
+
+# Filesystem Related
+IMAGE_DIR = os.path.join(_basedir, '/static/img/')
+THUMB_DIR = os.path.join(_basedir, '/static/img/thumb/')
 
 # Pagination
 PER_PAGE = 10
